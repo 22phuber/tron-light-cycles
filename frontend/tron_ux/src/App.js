4b2b112fa516ba97c79dc8643d0e91568bf6c3d9
huberpa4 <22phuber@gmail.com>
2020-03-29 13:09:53 +0200

    POC fixes

--- frontend/tron_ux/src/App.js
+++ frontend/tron_ux/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Frodo hallo
         </a>
       </header>
     </div>
