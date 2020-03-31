70c87bacb0057c53deb6c2a8b30c67e1929298ba
huberpa4 <22phuber@gmail.com>
2020-03-31 13:44:50 +0200

    gitignore change & websocket client component (WIP)

---
+++ frontend/tron_ux/src/components/canvas/canvas.component.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class GameCanvas extends Component {
+
+  componentDidMount() {
+    const canvas = this.refs.canvas;
+    const ctx = canvas.getContext("2d");
+  }
+
+  render() {
+    return (
+      <div>
+        <canvas ref="canvas" width={640} height={425} />
+      </div>
+    );
+  }
+
+}
+export default GameCanvas;
