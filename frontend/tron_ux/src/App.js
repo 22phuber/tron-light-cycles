e5ff1cbdf110a31e7cbc0ffcd8303fee85f045a4
huberpa4 <22phuber@gmail.com>
2020-03-31 20:36:54 +0200

    Dummy change2

--- frontend/tron_ux/src/App.js
+++ frontend/tron_ux/src/App.js
@@ -1,23 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import WSClient from "./components/wsClient/wsClient.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Frodo hallo
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn Frodo hallo
+          </a>
+        </header>
+      </div>
+      <div>
+        <WSClient />
+      </div>
     </div>
   );
 }
