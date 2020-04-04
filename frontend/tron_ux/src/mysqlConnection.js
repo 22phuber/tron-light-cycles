632bb66e11ebd1eb93659e4be146952cd512dea1
akcaden1 <akcaden1@students.zhaw.ch>
2020-04-04 19:52:18 +0200

    added addRider()

--- frontend/tron_ux/src/mysqlConnection.js
+++ frontend/tron_ux/src/mysqlConnection.js
@@ -20,11 +20,23 @@ mysqlConnection.prototype.getAllRiders = function(callback){
     });
 };
 
-mysqlConnection.prototype.getRiderById = function(id, callback){
+mysqlConnection.prototype.getRiderById = function(username, callback){
     con = this.mysql.createConnection(this.param);
     con.connect(function(err){
         if(err) throw err;
-        con.query("SELECT * FROM rider where id=?;", [id], function(err, result){
+        con.query("SELECT * FROM rider where username=?;", [username], function(err, result){
+            if(err) throw err;
+            con.end();
+            return callback(result);
+        });
+    });
+};
+
+mysqlConnection.prototype.addRider = function(username, firstname, name, password, email, cycle_color, ranking, callback){
+    con = this.mysql.createConnection(this.param);
+    con.connect(function(err){
+        if(err) throw err;
+        con.query("INSERT INTO rider VALUES ?", [username, firstname, name, password, email, cycle_color, ranking, callback], function(err, result){
             if(err) throw err;
             con.end();
             return callback(result);
