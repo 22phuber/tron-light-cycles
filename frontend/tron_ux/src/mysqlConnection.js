82e925e924c266246c7a49bc0069f0125d5a961d
akcaden1 <akcaden1@students.zhaw.ch>
2020-04-04 20:10:53 +0200

    fixed addRider()

--- frontend/tron_ux/src/mysqlConnection.js
+++ frontend/tron_ux/src/mysqlConnection.js
@@ -24,7 +24,7 @@ mysqlConnection.prototype.getRiderByUsername = function(username, callback){
     con = this.mysql.createConnection(this.param);
     con.connect(function(err){
         if(err) throw err;
-        con.query("SELECT * FROM rider where username=?;", [username], function(err, result){
+        con.query("SELECT * FROM rider WHERE username = ?;", [username], function(err, result){
             if(err) throw err;
             con.end();
             return callback(result);
@@ -36,7 +36,7 @@ mysqlConnection.prototype.addRider = function(username, firstname, name, passwor
     con = this.mysql.createConnection(this.param);
     con.connect(function(err){
         if(err) throw err;
-        con.query("INSERT INTO rider VALUES ?", [username, firstname, name, password, email, cycle_color, ranking, callback], function(err, result){
+        con.query("INSERT INTO rider (username, firstname, name, password, email, cycle_color, ranking) VALUES (?,?,?,?,?,?,?)", [username, firstname, name, password, email, cycle_color, ranking], function(err, result){
             if(err) throw err;
             con.end();
             return callback(result);
@@ -55,5 +55,3 @@ mysqlConnection.prototype.addRider = function(username, firstname, name, passwor
         console.log(result);
     });
 */
-
-
