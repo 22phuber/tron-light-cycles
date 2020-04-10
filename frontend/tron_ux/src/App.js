7debfa16b367f0700bb567fca94627d2f31d31fd
huberpa4 <22phuber@gmail.com>
2020-04-10 16:24:28 +0200

    Removed unused

--- frontend/tron_ux/src/App.js
+++ frontend/tron_ux/src/App.js
@@ -1,19 +1,5 @@
-import React, { Component } from "react";
-//import logo from './logo.svg';
+import React from "react";
 import "./App.css";
-import { Grid } from "@material-ui/core";
-import { withStyles } from "@material-ui/core/styles";
-import Initial from "./components/initial/initial";
-import Dialog from "@material-ui/core/Dialog";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import Login from "./components/login/login";
-import Register from "./components/register/register";
-
 import { ThemeProvider } from "@material-ui/styles";
 import { CssBaseline, Typography, createMuiTheme } from "@material-ui/core";
 import Container from "@material-ui/core/Container";
