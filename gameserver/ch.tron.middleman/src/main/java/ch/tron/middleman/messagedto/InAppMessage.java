b98aa7dd8ff27b3e0beaa1d6cd7e6f05dd69cfda
denadex <denizakca@outlook.com>
2020-04-22 12:43:57 +0200

    extend InAppMessage with abstract getGroupId() method

--- gameserver/ch.tron.middleman/src/main/java/ch/tron/middleman/messagedto/InAppMessage.java
+++ gameserver/ch.tron.middleman/src/main/java/ch/tron/middleman/messagedto/InAppMessage.java
@@ -4,4 +4,6 @@ package ch.tron.middleman.messagedto;
  * Represents a message that is being sent from
  * {@link ch.tron.game} to {@link ch.tron.transport} and vise versa.
  */
-public abstract class InAppMessage {}
+public abstract class InAppMessage {
+    public abstract String getGroupId();
+}
