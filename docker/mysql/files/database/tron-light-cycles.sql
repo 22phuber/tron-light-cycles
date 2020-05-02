8cf44cd6da1caf1bfc05aa33b29db17213f539b4
huberpa4 <22phuber@gmail.com>
2020-05-02 16:02:35 +0200

    Updated Database SQLs

--- docker/mysql/files/database/tron-light-cycles.sql
+++ docker/mysql/files/database/tron-light-cycles.sql
@@ -1,50 +1,81 @@
-/*
- * tron_light_cycles database
- */
-DROP DATABASE IF EXISTS tron_light_cycles;
+-- MySQL dump 10.13  Distrib 5.7.30, for Linux (x86_64)
+--
+-- Host: localhost    Database: tron_light_cycles
+-- ------------------------------------------------------
+-- Server version	5.7.30
 
-CREATE DATABASE `tron_light_cycles`;
-/*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */
-ALTER DATABASE tron_light_cycles CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-USE tron_light_cycles;
+--
+-- Current Database: `tron_light_cycles`
+--
 
-/*
- *	users table
- */
-Create Table: CREATE TABLE `users` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
-	`username` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-	`email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-	`password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-	`firstname` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-	`name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-	`cycle_color` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-	`ranking` bigint(20) unsigned DEFAULT NULL,
-	`createdAt` datetime NOT NULL,
-	`updatedAt` datetime NOT NULL,
-	PRIMARY KEY (`id`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci
-/*
- *	roles table
- */
-Create Table: CREATE TABLE `roles` (
-	`id` int(11) NOT NULL,
-	`name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-	`createdAt` datetime NOT NULL,
-	`updatedAt` datetime NOT NULL,
-	PRIMARY KEY (`id`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci
-/*
- *	user_roles table
- */
-Create Table: CREATE TABLE `user_roles` (
-	`createdAt` datetime NOT NULL,
-	`updatedAt` datetime NOT NULL,
-	`roleId` int(11) NOT NULL,
-	`userId` int(11) NOT NULL,
-	PRIMARY KEY (`roleId`, `userId`),
-	KEY `userId` (`userId`),
-	CONSTRAINT `user_roles_ibfk_1` FOREIGN KEY (`roleId`) REFERENCES `roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT `user_roles_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `tron_light_cycles` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `tron_light_cycles`;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `firstname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `cycle_color` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `ranking` bigint(20) unsigned DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_roles`
+--
+
+DROP TABLE IF EXISTS `user_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_roles` (
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `roleId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  PRIMARY KEY (`roleId`,`userId`),
+  KEY `userId` (`userId`),
+  CONSTRAINT `user_roles_ibfk_1` FOREIGN KEY (`roleId`) REFERENCES `roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `user_roles_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
