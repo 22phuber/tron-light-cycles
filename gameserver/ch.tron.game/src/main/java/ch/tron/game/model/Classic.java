74ee1f47edf5d35ff0c3c7d9830c0c9e41d2ba69
denadex <denizakca@outlook.com>
2020-05-03 13:18:42 +0200

    change enum to abstract class implement classic and extend gamemode

---
+++ gameserver/ch.tron.game/src/main/java/ch/tron/game/model/Classic.java
@@ -0,0 +1,13 @@
+package ch.tron.game.model;
+
+public class Classic extends GameMode{
+
+    public Classic(){
+        super(400,400,5);
+    }
+
+    @Override
+    public void start() {
+        
+    }
+}
--- gameserver/ch.tron.game/src/main/java/ch/tron/game/model/GameMode.java
+++ gameserver/ch.tron.game/src/main/java/ch/tron/game/model/GameMode.java
@@ -6,40 +6,34 @@ import java.util.Map;
 /**
  * Represents a tron game.
  */
-public enum GameMode {
+public abstract class GameMode {
 
-    CLASSIC("classic", 400, 400, 5);
+    final int x;
+    final int y;
+    final int lineThickness;
 
-    private final String name;
-    private final int x;
-    private final int y;
-    private final int lineThickness;
-
-    GameMode(String name, int x, int y, int lineThickness) {
-        this.name = name;
+    public GameMode(int x, int y, int lineThickness){
         this.x = x;
         this.y = y;
         this.lineThickness = lineThickness;
     }
 
-    public String getName() {
-        return name;
-    }
+    public abstract void start();
 
-    public int getX() {
+    public final int getX() {
         return x;
     }
 
-    public int getY() {
+    public final int getY() {
         return y;
     }
 
-    public int getLineThickness() {
+    public final int getLineThickness() {
         return lineThickness;
     }
 
-    public static GameMode getGameModeByName(String name) {
-        if (name.equals("classic")) { return GameMode.CLASSIC; }
+    public static final GameMode getGameModeByName(String name) {
+        if (name.equals("classic")) { return new Classic(); }
         return null;
     }
 }
