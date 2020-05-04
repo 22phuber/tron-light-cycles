ce9184bd68986b8adfe18bc6306d918eef0a9d5f
denadex <denizakca@outlook.com>
2020-05-04 11:21:45 +0200

    added walls moving closer to the center game mechanic

--- gameserver/ch.tron.game/src/main/java/ch/tron/game/model/BattleRoyal.java
+++ gameserver/ch.tron.game/src/main/java/ch/tron/game/model/BattleRoyal.java
@@ -8,6 +8,9 @@ import java.util.concurrent.TimeUnit;
 
 public class BattleRoyal extends GameMode{
 
+    private final int WALL_INTERVAL = 1000 * 10; //in milliseconds
+    private long lastWallInterval;
+
     public BattleRoyal(String lobbyId, Map<String, Player> players) {
         super(10000, 10000, 5, lobbyId, players);
     }
@@ -19,10 +22,13 @@ public class BattleRoyal extends GameMode{
         initialize();
         countdown();
 
+        lastWallInterval = System.currentTimeMillis();
+
         while (playersAlive.size() > 0) {
             now = System.nanoTime();
 
             move();
+            moveWalls();
             render();
 
             delta = System.nanoTime() - now;
@@ -40,4 +46,13 @@ public class BattleRoyal extends GameMode{
     public void move() {
         //gameplay logic comes in here
     }
+
+    public void moveWalls(){
+        //move the outer walls closer to the center every "WALL_INTERVAL"
+        if(System.currentTimeMillis() - lastWallInterval >= WALL_INTERVAL){
+            //move walls closer to the center
+            lastWallInterval = System.currentTimeMillis();
+        }
+    }
+
 }
