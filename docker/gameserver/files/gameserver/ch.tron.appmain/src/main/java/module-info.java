f380ff6fc7a3a5a00f40a522b77c60893b44df09
huberpa4 <22phuber@gmail.com>
2020-05-08 13:24:22 +0200

    added gitignore

--- docker/gameserver/files/gameserver/ch.tron.appmain/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module ch.tron.appmain {
-
-    // slf4j
-    requires org.slf4j;
-
-    // Tron
-    requires ch.tron.game;
-    requires ch.tron.transport;
-}
--- docker/gameserver/files/gameserver/ch.tron.middleman/src/main/java/ch/tron/middleman/ToGameMessageService.java
+++
@@ -1,7 +0,0 @@
-package ch.tron.middleman;
-import ch.tron.middleman.messagedto.*;
-
-public interface ToGameMessageService {
-    void handle(InAppMessage msg);
-}
-
