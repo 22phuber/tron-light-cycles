74e8cbe2535f58ae164cb5f4e7e45aade16fb16d
denadex <denizakca@outlook.com>
2020-05-10 19:08:31 +0200

    create new java classes all used in the battleroyal mode

---
+++ gameserver/ch.tron.game/src/main/java/ch/tron/game/model/Wall.java
@@ -0,0 +1,27 @@
+package ch.tron.game.model;
+
+public class Wall {
+    private int width;
+    private int height;
+
+    public Wall(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
