cad3be51e7c61e9c67d1b8cd7e90af216fa58e86
denadex <denizakca@outlook.com>
2020-05-19 11:00:22 +0200

    add Tests for Models

---
+++ gameserver/ch.tron.game/src/test/java/ch/tron/game/model/PlayerTest.java
@@ -0,0 +1,84 @@
+package ch.tron.game.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    private Player player;
+
+    @BeforeEach
+    void initialize(){
+        player = new Player("playerId", "playerName", "rgb(0,0,0)");
+    }
+
+    @Test
+    void addTurn() {
+    }
+
+    @Test
+    void getId() {
+        assertEquals("playerId", player.getId());
+    }
+
+    @Test
+    void getPosx() {
+    }
+
+    @Test
+    void setPosx() {
+    }
+
+    @Test
+    void getPosy() {
+    }
+
+    @Test
+    void setPosy() {
+    }
+
+    @Test
+    void getDir() {
+    }
+
+    @Test
+    void setDir() {
+    }
+
+    @Test
+    void getColor() {
+        assertEquals("rgb(0,0,0)", player.getColor());
+    }
+
+    @Test
+    void setColor() {
+    }
+
+    @Test
+    void isReady() {
+        assertFalse(player.isReady());
+    }
+
+    @Test
+    void setReady() {
+    }
+
+    @Test
+    void getReady() {
+    }
+
+    @Test
+    void getTurns() {
+    }
+
+    @Test
+    void getName() {
+        assertEquals("playerName", player.getName());
+    }
+
+    @Test
+    void setName() {
+    }
+}
---
+++ gameserver/ch.tron.game/src/test/java/ch/tron/game/model/WallTest.java
@@ -0,0 +1,60 @@
+package ch.tron.game.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WallTest {
+
+    private Wall wall;
+
+    @BeforeEach
+    void initialize(){
+        wall = new Wall(100, 100, 0, 0);
+    }
+
+    @Test
+    void getWidth() {
+        assertEquals(100, wall.getWidth());
+    }
+
+    @Test
+    void setWidth() {
+        wall.setWidth(200);
+        assertEquals(200, wall.getWidth());
+    }
+
+    @Test
+    void getHeight() {
+        assertEquals(100, wall.getHeight());
+    }
+
+    @Test
+    void setHeight() {
+        wall.setHeight(200);
+        assertEquals(200, wall.getHeight());
+    }
+
+    @Test
+    void getX() {
+        assertEquals(0, wall.getX());
+    }
+
+    @Test
+    void setX() {
+        wall.setX(1);
+        assertEquals(1, wall.getX());
+    }
+
+    @Test
+    void getY() {
+        assertEquals(0, wall.getY());
+    }
+
+    @Test
+    void setY() {
+        wall.setY(1);
+        assertEquals(1, wall.getY());
+    }
+}
